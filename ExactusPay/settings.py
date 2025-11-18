141fdab042d41b14753853cf02effcd703b2fe7a
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-18 08:27:37 +0000

    fix the database reset

--- ExactusPay/settings.py
+++ ExactusPay/settings.py
@@ -93,19 +93,17 @@ CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 # Database (works for now)
 if "DATABASE_URL" in os.environ:
-    # Production: use Render PostgreSQL
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
-            ssl_require=not DEBUG,   # usually True in production
+            ssl_require=True,
         )
     }
 else:
-    # Local dev fallback: SQLite
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "NAME": BASE_DIR / 'db.sqlite3'
         }
     }
 
