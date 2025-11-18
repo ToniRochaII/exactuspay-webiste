65f977eb7ae6e1ef7f561aa42e3c76b549459902
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-18 14:31:29 +0000

    fixing company_delete process

--- company/urls.py
+++ company/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
 
     path("<slug:country_slug>/", views.company, name="company"),
     path("<slug:country_slug>/create/", views.company_create, name="company_create"),
-    path("<slug:country_slug>/delete/<int:company_id>/", views.company_delete, name="company_delete"),
+    path("<slug:country_slug>/delete", views.company_delete, name="company_delete"),
     path("<slug:country_slug>/edit/<int:company_id>/", views.company_edit, name="company_edit"),
 
 ]
