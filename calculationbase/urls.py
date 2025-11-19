df652d8d3c83bb9608cb5028af4dccf1bbcfb5f6
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-19 11:18:45 +0000

    calculationbase link 5

--- calculationbase/urls.py
+++ calculationbase/urls.py
@@ -6,6 +6,6 @@ app_name = "calculationbase"
 urlpatterns = [
     path("<slug:country_slug>/calculationbase/<int:regulations_id>/", views.calculationbase_list, name="list"),
     path("<slug:country_slug>/calculationbase/<int:regulations_id>/create/", views.calculationbase_create, name="create"),
-    path("<slug:country_slug>/calculationbase/edit/<int:pk>/", views.calculationbase_edit, name="edit"),
-    path("<slug:country_slug>/calculationbase/delete/<int:pk>/", views.calculationbase_delete, name="delete"),
+    path("<slug:country_slug>/calculationbase/edit/<int:regulations_id>/", views.calculationbase_edit, name="edit"),
+    path("<slug:country_slug>/calculationbase/delete/<int:regulations_id>/", views.calculationbase_delete, name="delete"),
 ]
