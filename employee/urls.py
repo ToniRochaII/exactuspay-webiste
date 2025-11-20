e432cfdad0761f8548414431a15d39d47f51268c
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-20 09:58:12 +0000

    employeee

--- employee/urls.py
+++ employee/urls.py
@@ -5,7 +5,7 @@ app_name = "employee"
 
 urlpatterns = [
 
-    path("<int:company_id>/", views.employee_list, name="employee"),
+    path("<slug:country_slug>/<int:company_id>/", views.employee_list, name="employee"),
     path("<int:company_id>/create/", views.employee_create, name="employee_create"),
     path("<int:company_id>/edit/<int:employee_id>/", views.employee_edit, name="employee_edit"),
 
