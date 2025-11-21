231068e34de5948c73c2869648dacc2348b6527d
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-21 14:09:25 +0000

    pdcodes

--- payregister/urls.py
+++ payregister/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 from . import views
 
-app_name = 'payregister'
+app_name = "payregister"
 
 urlpatterns = [
-    path('<int:employee_id>/', views.list_entries, name='payregister_list'),
-    path('<int:employee_id>/create/', views.create_entry, name='payregister_create'),
+    path(
+        "payregister/<int:employee_id>/",
+        views.list_entries,
+        name="payregister_list",
+    ),
+    path(
+        "payregister/<int:employee_id>/create/",
+        views.create_entry,
+        name="payregister_create",
+    ),
 ]
+
