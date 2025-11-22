302de54c2881883d2a8c1ce8cccdc7eb75595b5c
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-22 13:59:02 +0000

    pdcodes

--- payroll/urls.py
+++ payroll/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 
     path("<slug:country_slug>/<int:company_id>/payroll", views.payroll, name="payroll"),
     path("<slug:country_slug>/<int:company_id>/payroll/create/", views.payroll_create, name="payroll_create"),
-    path("<slug:country_slug>/<int:company_id>/payroll/edit/<int:payroll_id>/", views.payroll_edit, name="payroll_edit"),
-    path("<slug:country_slug>/<int:company_id>/payroll/delete/<int:payroll_id>/", views.payroll_delete, name="payroll_delete"),
+    path("<slug:country_slug>/<int:company_id>/payroll/edit/<int:pk>/", views.payroll_edit, name="payroll_edit"),
+    path("<slug:country_slug>/<int:company_id>/payroll/delete/<int:pk>/", views.payroll_delete, name="payroll_delete"),
 
 ]
 
