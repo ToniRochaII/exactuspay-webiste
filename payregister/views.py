5735ee92b041fa6917812a9baa4b55b48d3f1cba
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-23 16:57:26 +0000

    pdcodes

--- payregister/views.py
+++ payregister/views.py
@@ -2,9 +2,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from employee.models import Employee
 from payregister.models import PayRegister
 from payregister.forms import PayRegisterForm
-from company.models import Company
+from company.models import  Company
 from country.models import Country
 
+def list_entries(request, country_slug, company_id, id):
+    country = get_object_or_404(Country, slug=country_slug)
+    company = get_object_or_404(Company, pk=company_id)
+    employee = get_object_or_404(Employee, id=id)
+    entries = employee.payregister_entries.all()
+    
+    return render(request, 'payregister/list.html', {
+        'employee': employee,
+        'entries': entries,
+        'company': company,
+        'country': country,
+        'country_slug': country_slug,
+        
+    })
 
 def create_entry(request, country_slug, company_id, id):
     country = get_object_or_404(Country, slug=country_slug)
@@ -38,3 +52,9 @@ def create_entry(request, country_slug, company_id, id):
             "country_slug": country_slug,
         },
     )
+
+
+
+
+
+
