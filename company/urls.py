f8b222e5ba4237ab303a3725eca825fc367f81ff
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-26 17:16:20 +0000

    pdcodes

--- company/urls.py
+++ company/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     # ────────────────────────────────────────────────
     # CRUD routes
     # ────────────────────────────────────────────────
-    path("<slug:country_slug>/", views.company, name="company"),
-    path("<slug:country_slug>/create/", views.company_create, name="company_create"),
+    path("country/", views.company, name="company"),
+    path("country/create/", views.company_create, name="company_create"),
     path("<slug:country_slug>/delete", views.company_delete, name="company_delete"),
     path("<slug:country_slug>/edit/<int:company_id>/", views.company_edit, name="company_edit"),
 ]
