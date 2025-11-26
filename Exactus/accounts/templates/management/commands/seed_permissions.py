fe87abe1255b606bb128a6e1fc31bf6ee7b1ec33
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-26 21:39:01 +0000

    pdcodes

--- Exactus/accounts/templates/management/commands/seed_permissions.py
+++ Exactus/accounts/templates/management/commands/seed_permissions.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from accounts.models import PermissionMatrix
+from Exactus.accounts.models import PermissionMatrix
 
 BASE = {
     "ADMIN": [("ALL", "ALL")],
