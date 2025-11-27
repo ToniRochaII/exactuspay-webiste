a20e0bde5a1c8e6f42f4a2821ca9a9e93d285106
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-27 10:16:26 +0000

    pdcodes

--- ExactusPay/settings.py
+++ ExactusPay/settings.py
@@ -143,9 +143,9 @@ PROGRESS_SESSION_KEY = 'upload_progress'
 
 AUTH_USER_MODEL = 'accounts.User'
 
-LOGIN_URL = "/login/"
+LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
-LOGOUT_REDIRECT_URL = "/login/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
 
 LANGUAGE_CODE = "en-gb"
 TIME_ZONE = 'UTC'
