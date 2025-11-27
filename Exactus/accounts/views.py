f37f40f3de15cb3d49c16021d2e7198343638c2b
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-27 10:57:03 +0000

    pdcodes

--- Exactus/accounts/views.py
+++ Exactus/accounts/views.py
@@ -129,7 +129,7 @@ def custom_login(request):
         else:
             messages.error(request, 'Invalid username or password.')
     
-    return render(request, 'accounts/login.html')
+    return render(request, 'auth/login.html')
 
 @login_required
 def profile(request):
