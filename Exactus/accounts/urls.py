6f3f25ea6764a53ddc976422caca9f5692c2f2c1
Toni Rocha <antoniorocha@exactuspay.com>
2025-11-27 09:33:38 +0000

    pdcodes

--- Exactus/accounts/urls.py
+++ Exactus/accounts/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
 
 
     path('register/', views.register, name='register'),
-    path('login/', views.custom_login, name='login'),
-    path('logout/', views.custom_logout, name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name="accounts/login.html",redirect_authenticated_user=True), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
 
     # Password reset
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='auth/password_reset.html'), name='password_reset'),
