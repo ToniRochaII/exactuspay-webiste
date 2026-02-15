8e613623654585a80944dfec774f1ef76dcc44a5
Toni Rocha <antoniorocha@exactuspay.com>
2026-02-15 18:54:36 +0000

    Resolving bugs

--- website/ExactusPay/settings.py
+++ website/ExactusPay/settings.py
@@ -128,13 +128,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
 
 
 
-USE_TZ = True
+
+
+
+
 
 
 # Static files (CSS, JavaScript, Images)
