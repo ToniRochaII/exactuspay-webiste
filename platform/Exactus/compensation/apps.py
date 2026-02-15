1d2b61d534acc0521a41da0e652ba82f822b4d10
Toni Rocha <antoniorocha@exactuspay.com>
2026-02-15 14:10:58 +0000

    Website branch: marketing website only

--- platform/Exactus/compensation/apps.py
+++
@@ -1,8 +0,0 @@
-# Exactus/compensation/apps.py
-from django.apps import AppConfig
-
-
-class CompensationConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "Exactus.compensation"
-    verbose_name = "Compensation"
--- platform/Exactus/regulations/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RegulationsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Exactus.regulations'
