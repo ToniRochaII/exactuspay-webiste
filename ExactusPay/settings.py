bd92df1efaf54bd2ba9682e2643511b711f254a7
Toni Rocha <antoniorocha@exactuspay.com>
2026-03-12 08:49:07 +0000

    Merge remote website branch

--- ExactusPay/settings.py
+++ ExactusPay/settings.py
@@ -112,7 +112,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 EMAIL_HOST = "mta.extendcp.co.uk"
-EMAIL_PORT = 4587
+EMAIL_PORT = 587
 
 EMAIL_USE_SSL = False
 EMAIL_USE_TLS = True
