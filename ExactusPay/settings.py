ed5f5f697304dfb597ffe1d3c5357d1296a73c28
Toni Rocha <antoniorocha@exactuspay.com>
2026-03-21 17:37:12 +0000

    testimonial

--- ExactusPay/settings.py
+++ ExactusPay/settings.py
@@ -113,7 +113,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # -----------------------------------------------------------------------------
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "mta.extendcp.co.uk")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.office365.com")
 EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "587"))
 
 EMAIL_USE_TLS = True
